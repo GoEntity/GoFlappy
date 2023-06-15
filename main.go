7a09a0ac877c7ff4edf83bfe5bb3783c9fb62f17
GoEntity <javaLearner13@gmail.com>
2023-06-16 04:32:16 +0900

    init

--- main.go
+++ main.go
@@ -1,9 +1,226 @@
 package main
 
-import(
+import (
+	"image/color"
+	"log"
 
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
 )
 
-func main(){
+const (
+	screenWidth  = 1280
+	screenHeight = 960
+
+	backgroundWidth = 1280 * 4
+	scrollSpeed     = 2
+
+	menuOption_normal = iota
+	menuOption_fast
+	menuOption_faster
+
+	GameStateMenu GameState = iota
+	GameStatePlaying
+)
+
+var (
+	player            *ebiten.Image
+	background        *ebiten.Image
+	scrollOffset      = 0
+	currentMenuOption = menuOption_normal
+
+	playerPosition  = vector{X: 100, Y: 350}
+	playerVelocity  = vector{}
+	playerScreenPos = vector{}
+
+	continuousMovement = 0
+	gravity            = 1
+	jumpPower          = 10
+	playerSpeed        = 2
+	maxSpeed = 5
+)
+
+var menuOptions = []string{"NORMAL", "FAST", "FASTER"}
+
+type vector struct {
+	X, Y float64
+}
+
+type GameState int
+
+type Game struct {
+	state GameState
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	switch g.state {
+	case GameStateMenu:
+		g.drawMenu(screen)
+	case GameStatePlaying:
+		g.drawPlaying(screen)
+	}
+}
+
+func (g *Game) drawMenu(screen *ebiten.Image) {
+	ebitenutil.DrawRect(screen, 0, 0, screenWidth, screenHeight, color.RGBA{R: 0, G: 0, B: 80, A: 200})
+	ebitenutil.DebugPrintAt(screen, "GOPHER FLOPPY", screenWidth/2 - 70, screenHeight/2 - 80)
+	for i, pick := range menuOptions {
+		textX := screenWidth/2 - len(pick)*6
+		textY := screenHeight/2 + (i * 50)
+		if i == currentMenuOption {
+			ebitenutil.DebugPrintAt(screen, "=> "+pick, textX, textY)
+		} else {
+			ebitenutil.DebugPrintAt(screen, pick, textX, textY)
+		}
+	}
+}
+
+func (g *Game) drawPlaying(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(-scrollOffset), 0)
+	screen.DrawImage(background, op)
+	op = &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(playerScreenPos.X, playerScreenPos.Y)
+	screen.DrawImage(player, op)
+}
+
+func (g *Game) updateMenu(screen *ebiten.Image) {
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyDown) {
+		currentMenuOption++
+		if currentMenuOption >= len(menuOptions) {
+			currentMenuOption = len(menuOptions) - 1
+		}
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyUp) {
+		currentMenuOption--
+		if currentMenuOption < 0 {
+			currentMenuOption = 0
+		}
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+		switch currentMenuOption {
+		case 0:
+			g.state = GameStatePlaying
+			// playerPosition = playerPosition
+			
+		case 1:
+			g.state = GameStatePlaying
+			// playerPosition = playerPosition
+			gravity*=2
+			jumpPower*=2
+			playerSpeed*=2
+			maxSpeed*=2
+		case 2:
+			g.state = GameStatePlaying
+			// playerPosition = playerPosition
+			gravity*=3
+			jumpPower*=3
+			playerSpeed*=3
+			maxSpeed*=3
+		}
+	}
+}
+
+func (g *Game) updatePlaying() {
+	handleGameInput()
+	movePlayer()
+}
+
+func handleGameInput() {
+	continuousMovement = 0
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		continuousMovement = -1
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		continuousMovement = 1
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		playerVelocity.Y = -float64(jumpPower)
+	}
+}
+
+func movePlayer() {
+	playerVelocity.Y += float64(gravity)
+	if playerVelocity.Y > float64(maxSpeed){
+		playerVelocity.Y = float64(maxSpeed)
+	}
+	playerVelocity.X = float64(continuousMovement) * float64(playerSpeed)
 	
+	targetPlayerPosition := vector{
+		X: playerPosition.X + playerVelocity.X,
+		Y: playerPosition.Y + playerVelocity.Y,
+	}
+
+	playerPosition = targetPlayerPosition
+
+	playerScreenPos.X = playerPosition.X - float64(scrollOffset)
+	playerScreenPos.Y = playerPosition.Y
+
+	if playerPosition.X < 0 {
+		playerPosition.X = 0
+	} else if playerPosition.X > float64(backgroundWidth - player.Bounds().Dx()){
+		playerPosition.X = float64(backgroundWidth - player.Bounds().Dx())
+	}
+
+	if playerPosition.Y > screenHeight - 70 {
+		playerPosition.Y = screenHeight - 70
+		playerVelocity.Y = 0
+	} else if playerPosition.Y < 0 {
+		playerPosition.Y = 0
+		playerVelocity.Y = 0
+	}
+
+	scrollOffset = int(playerPosition.X) - screenWidth/2
+	if scrollOffset < 0 {
+		scrollOffset = 0
+	} else if scrollOffset > backgroundWidth - screenWidth {
+		scrollOffset = backgroundWidth - screenWidth
+	}
+
+}
+
+func (g *Game) Update(screen *ebiten.Image) error {
+	switch g.state {
+	case GameStateMenu:
+		g.updateMenu(screen)
+	case GameStatePlaying:
+		g.updatePlaying()
+	}
+
+	g.Draw(screen)
+
+	return nil
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	var err error
+	player, _, err = ebitenutil.NewImageFromFile("player.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	background, _, err = ebitenutil.NewImageFromFile("background.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	currentMenuOption = 0
+
+	game := &Game{
+		state: GameStateMenu,
+	}
+
+	ebiten.SetWindowTitle("GopherFloppy")
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
